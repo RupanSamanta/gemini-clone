9e2e29159f505ad60d1eaccc52e97f72e5835c60
Rupan Samanta <rvsjan32@gmail.com>
2026-02-16 00:26:38 +0530

    added styling to quick cards

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,4 +1,5 @@
-import { FaRegCompass } from "react-icons/fa6";
+import { FaRegCompass, FaRegLightbulb, FaCode } from "react-icons/fa6";
+import { LuNotebookText } from "react-icons/lu";
 
 const Main = () => {
     return (
@@ -7,29 +8,28 @@ const Main = () => {
                 <span className="main">Gemini</span>
                 <span className="user-image inline-block size-10 border rounded-full bg-[url(/user_icon.png)] bg-cover bg-no-repeat bg-center" />
             </nav>
-            <div className="main-container">
-                <div className="greet">
-                    <p><span>Hello, Dev.</span></p>
+            <div className="main-container max-w-225 m-auto ">
+                <div className="greet m-[50px 0] text-[56px] text-[#c4c7c5] font-medium p-5">
+                    <p><span className="bg-size-[100%] bg-linear-16 from-[#4b90ff] to-[#ff5546] bg-clip-text text-transparent">Hello, Dev.</span></p>
                     <p>How can I help you today?</p>
                 </div>
-                <div className="cards">
+                <div className="cards grid grid-cols-[repeat(auto-fill,minmax(180px,1fr))] gap-3.75 p-5">
                     <div className="card">
                         <p>Suggest beautiful places to visit</p>
                         <FaRegCompass />
+                    </div>
+                    <div className="card">
+                        <p>Briefly summarize the concept : Generative AI</p>
+                        <LuNotebookText />
                     </div>
                     
                     <div className="card">
-                        <p>Suggest beautiful places to visit</p>
-                        <FaRegCompass />
-                    </div>
-                    
-                    <div className="card">
-                        <p>Suggest beautiful places to visit</p>
-                        <FaRegCompass />
+                        <p>Brainstorm team bonding activities for our work team</p>
+                        <FaRegLightbulb />
                     </div>
                     <div className="card">
-                        <p>Suggest beautiful places to visit</p>
-                        <FaRegCompass />
+                        <p>Improve the readability of the code</p>
+                        <FaCode />
                     </div>
                 </div>
             </div>
