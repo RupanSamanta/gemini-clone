1ddd56160e349eabe5d1570c138a6d2796d8b001
Rupan Samanta <rvsjan32@gmail.com>
2026-02-18 19:28:42 +0530

    added query text box

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,11 +1,12 @@
 import { FaRegCompass, FaRegLightbulb, FaCode } from "react-icons/fa6";
-import { LuNotebookText } from "react-icons/lu";
+import { LuNotebookText, LuMic, LuSendHorizontal } from "react-icons/lu";
+import { MdOutlineAddPhotoAlternate } from "react-icons/md";
 
 const Main = () => {
     return (
         <div className="main flex-1 min-h-screen pb-[15vh] relative">
             <nav className="navbar flex items-center justify-between text-[22px] p-5 text-[#585858]">
-                <span className="main">Gemini</span>
+                <span className="main font-medium">Gemini</span>
                 <span className="user-image inline-block size-10 border rounded-full bg-[url(/user_icon.png)] bg-cover bg-no-repeat bg-center" />
             </nav>
             <div className="main-container max-w-225 m-auto ">
@@ -22,7 +23,7 @@ const Main = () => {
                         <p>Briefly summarize the concept : Generative AI</p>
                         <LuNotebookText />
                     </div>
-                    
+
                     <div className="card">
                         <p>Brainstorm team bonding activities for our work team</p>
                         <FaRegLightbulb />
@@ -30,6 +31,19 @@ const Main = () => {
                     <div className="card">
                         <p>Improve the readability of the code</p>
                         <FaCode />
+                    </div>
+                </div>
+                <div className="main-bottom absolute bottom-0 w-full max-w-225 p-[0_2px] m-auto">
+                    <div className="search-box flex items-center justify-between gap-5 bg-[#f0f4f9] rounded-[50px] p-[10px 20px]">
+                        <input type="text" name="search" id="search-input" placeholder="Ask anything..." />
+                        <div className="&> w-6 cursor-pointer">
+                            <MdOutlineAddPhotoAlternate />
+                            <LuMic />
+                            <LuSendHorizontal />
+                        </div>
+                    </div>
+                    <div className="bottom-info">
+                        <p>Gemini may display inaccurate info, including about people, so double-check its responses. Your privacy and Gemini Apps</p>
                     </div>
                 </div>
             </div>
